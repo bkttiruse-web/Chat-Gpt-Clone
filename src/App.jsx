d3185d3acfe284bee22c1642fade91d17f1d7aca
Biruck0 <bkttiruse@gmail.com>
2026-08-12 01:29:47 +0300

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import ChatArea from "./components/ChatArea";
 
 function App() {
+  const [selectedChat, setSelectedChat] = useState(null);
+
   return (
     <div className="app">
-      <Sidebar />
-      <ChatArea />
+      <Sidebar selectedChat={selectedChat} onSelectChat={setSelectedChat} />
+
+      <ChatArea selectedChat={selectedChat} />
     </div>
   );
 }
